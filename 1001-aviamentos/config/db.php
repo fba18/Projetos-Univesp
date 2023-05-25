6a6256ea765e01b0bced30006092abf639128f8b
fba18 <fernando.alves_cad@outlook.com.br>
2023-05-25 11:33:50 -0300

    Alterado conexão do banco de dados

--- 1001-aviamentos/config/db.php
+++ 1001-aviamentos/config/db.php
@@ -2,9 +2,12 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=1001_aviamentos',
-    'username' => 'root',
-    'password' => '1234',
+    'dsn' => 'mysql:host=us-cdbr-east-06.cleardb.net;dbname=heroku_653aa8b3b9abedf',
+    'username' => 'bcb24a29bc85a8',
+    'password' => '462454b5',
+    //'dsn' => 'mysql:host=localhost;dbname=1001_aviamentos',
+    //'username' => 'root',
+    //'password' => '1234',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
